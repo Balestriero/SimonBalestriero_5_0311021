04fae9058827fd2f754fd2c2bd948ab606b348c2
balestriero <simon.balestriero@gmail.com>
2021-12-09 13:44:20 +0100

    ajout nom+desc sur cards

--- site/front/js/script.js
+++ site/front/js/script.js
@@ -8,24 +8,32 @@ fetch("//localhost:3000/api/products")
     const items = document.getElementById("items");
     for (let i = 0; i < value.length; i++) {
       //ajout des liens
-
       const newItem = document.createElement("a");
-      // newItem.setAttribute("class", "item");
       newItem.setAttribute("href", `./product.html?id=${value[i]._id}`);
       items.appendChild(newItem);
 
       //ajout des articles dans les liens
-      // const item = document.getElementsByClassName("item");
       const newArticleInItem = document.createElement("article");
-      newArticleInItem.setAttribute("class", "article");
       newItem.appendChild(newArticleInItem);
 
       // //ajout des images dans les articles
-      // const articleInItem = document.getElementsByClassName("article");
       const newImgInArticle = document.createElement("img");
       newImgInArticle.setAttribute("src", `${value[i].imageUrl}`);
       newImgInArticle.setAttribute("alt", `${value[i].altTxt}`);
       newArticleInItem.appendChild(newImgInArticle);
+
+      // //ajout des noms dans les articles
+      const newNameInArticle = document.createElement("h3");
+      newNameInArticle.setAttribute("class", "productDescription");
+      newNameInArticle.textContent = `${value[i].name}`;
+      newArticleInItem.appendChild(newNameInArticle);
+
+      // `<h3 class="productName">${value[i].name}</h3>`;
+
+      const newDescriptionInArticle = document.createElement("p");
+      newDescriptionInArticle.setAttribute("class", "productDescription");
+      newDescriptionInArticle.textContent = `${value[i].description}`;
+      newArticleInItem.appendChild(newDescriptionInArticle);
     }
   })
   .catch(function (err) {
